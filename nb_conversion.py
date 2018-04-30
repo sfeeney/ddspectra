e468ba4d0bcaced9ab209ebfaccbfe7397b37c02
Stephen Feeney <sfeeney@simonsfoundation.org>
2018-04-30 12:12:52 -0400

    Fix MPI random seeds. Add version of code in which signal covariancec is never inverted in constructing the Wiener Filter: ill-conditioned cov mats can still break the inv wish sample though. Therefore also add ability to specify regularizing noise.

--- nb_conversion.py
+++ nb_conversion.py
@@ -28,7 +28,9 @@ def allocate_jobs(n_jobs, n_procs=1, rank=0):
 def complete_array(target_distrib, use_mpi=False):
 	if use_mpi:
 		target = np.zeros(target_distrib.shape)
-		mpi.COMM_WORLD.Allreduce(target_distrib, target, op=mpi.SUM)
+		mpi.COMM_WORLD.Reduce(target_distrib, target, op=mpi.SUM, \
+							  root=0)
+		#mpi.COMM_WORLD.Allreduce(target_distrib, target, op=mpi.SUM)
 	else:
 		target = target_distrib
 	return target
@@ -48,77 +50,144 @@ cm = mpcm.get_cmap('plasma')
 
 # setup
 use_mpi = True
-n_bins = 100 # 50
+constrain = True
+no_s_inv = False
+n_bins = 100 # 7 # 50
 n_spectra = 2000
-n_samples = 50
+n_samples = 1000
 n_warmup = n_samples / 4
 n_gp_reals = 50
 jeffreys_prior = 1
 diagnose = False
 datafile = 'data/redclump_1_alpha_nonorm.h5' # filename or None
+inf_noise = 1.0e5
+reg_noise = 1.0e-6
 
 # set up identical within-chain MPI processes
 if use_mpi:
 	import mpi4py.MPI as mpi
 	n_procs = mpi.COMM_WORLD.Get_size()
 	rank = mpi.COMM_WORLD.Get_rank()
-	seed = np.random.randint(102314, 221216, 1)[0]
-	seed = mpi.COMM_WORLD.bcast(seed, root=0)
-	npr.seed(seed)
 else:
 	n_procs = 1
 	rank = 0
+if constrain:
+	seed = 231014 + rank
+else:
+	seed = npr.randint(231014, 221216, 1)[0]
+npr.seed(seed)
 
 # simulate or read input data
 if datafile is None:
 
-	# generate a covariance matrix
-	norm_vars = npr.randn(n_bins)
-	in_mat = np.zeros((n_bins, n_bins))
-	for i in range(n_bins):
-		in_mat[:, i] = norm_vars
-	u, s, v = npl.svd(in_mat)
-	evex = u
-	evals = np.linspace(1.0, n_bins, n_bins) ** -5
-	cov = np.dot(np.dot(evex, np.diag(evals)), evex.T)
-	evals, evex = npl.eigh(cov)
-	if diagnose:
-		mp.imshow(cov, interpolation='nearest')
-		mp.show()
+	# initialize
+	data = np.zeros((n_spectra, n_bins))
+	mask = np.zeros((n_spectra, n_bins))
+	if no_s_inv:
+		cov_noise = np.zeros((n_spectra, n_bins))
+	else:
+		inv_cov_noise = np.zeros((n_bins, n_bins))
+	wl = np.zeros(n_bins)
+	mean = np.zeros(n_bins)
+	cov = np.zeros((n_bins, n_bins))
+	if rank == 0:
 
-	# generate a mean vector
-	mean = 1.0 + 4.0 * (npr.rand(n_bins) - 0.5)
-	if diagnose:
-		mp.plot(mean)
-		mp.show()
+		# additional setup: use BW setup or test input matching prior
+		test_match = False
+		if test_match:
 
-	# generate signals from this model
-	spectra_true = npr.multivariate_normal(mean, cov, n_spectra)
-	if diagnose:
-		print spectra_true.shape
-		for i in range(n_spectra):
-			mp.plot(spectra_true[i, :], 'k-', alpha=0.3)
-		mp.show()
+			# generate a covariance matrix: HOW? not inv wishart prior, 
+			# but something about determinant. so how do we guarantee 
+			# that?
+			norm_vars = npr.randn(n_bins)
+			in_mat = np.zeros((n_bins, n_bins))
+			for i in range(n_bins):
+				in_mat[:, i] = norm_vars
+			u, s, v = npl.svd(in_mat)
+			evex = u
+			evals = np.linspace(1.0, n_bins, n_bins) ** -5
+			cov = np.dot(np.dot(evex, np.diag(evals)), evex.T)
+			evals, evex = npl.eigh(cov)
 
-	# generate noise
-	var_noise = npr.uniform(0.0, 0.2, n_bins)
-	cov_noise = np.diag(var_noise)
-	noise = npr.multivariate_normal(np.zeros(n_bins), cov_noise, n_spectra)
-	inv_cov_noise = npl.inv(cov_noise)      # inverseNoiseCov
-	if diagnose:
-		for i in range(n_spectra):
-			mp.plot(noise[i, :], 'k-', alpha=0.3)
-		mp.show()
+			# generate a mean vector
+			mean = npr.uniform(0.5, 1.5, n_bins)
 
-	# use a simple mask
-	mask = np.reshape(npr.choice(2, n_bins * n_spectra, p=[0.14, 0.86]), \
-					  spectra_true.shape)
-	data = mask * (spectra_true + noise)
-	wl = np.arange(n_bins)
-	if diagnose:
-		print float(np.sum(mask==1.0)) / float(np.sum(mask==0.0)), 0.86/0.14
-		mp.imshow(data, interpolation='nearest')
-		mp.show()
+		else:
+
+			# generate a covariance matrix
+			norm_vars = npr.randn(n_bins)
+			in_mat = np.zeros((n_bins, n_bins))
+			for i in range(n_bins):
+				in_mat[:, i] = norm_vars
+			u, s, v = npl.svd(in_mat)
+			evex = u
+			evals = np.linspace(1.0, n_bins, n_bins) ** -5
+			#evals = np.linspace(1.0, n_bins, n_bins) ** -2
+			#evals = np.linspace(1.0, n_bins, n_bins)
+			cov = np.dot(np.dot(evex, np.diag(evals)), evex.T)
+			#cov = np.diag(np.diag(cov))
+			evals, evex = npl.eigh(cov)
+
+			# generate a mean vector
+			mean = 1.0 + 4.0 * (npr.rand(n_bins) - 0.5)
+		print 'input covariance condition number ', npl.cond(cov)
+
+		# optionally plot test inputs
+		if diagnose:
+			mp.imshow(cov, interpolation='nearest')
+			mp.show()
+			mp.plot(mean)
+			mp.show()
+
+		# generate signals from this model
+		spectra_true = npr.multivariate_normal(mean, cov, n_spectra)
+		if diagnose:
+			print spectra_true.shape
+			for i in range(n_spectra):
+				mp.plot(spectra_true[i, :], 'k-', alpha=0.3)
+			mp.show()
+
+		# generate noise and mask
+		var_noise = npr.uniform(0.0, 0.2, n_bins)
+		cov_noise = np.diag(var_noise)
+		inv_cov_noise = npl.inv(cov_noise)      # inverseNoiseCov
+		noise = npr.multivariate_normal(np.zeros(n_bins), cov_noise, n_spectra)
+		mask = np.reshape(npr.choice(2, n_bins * n_spectra, p=[0.14, 0.86]), \
+						  spectra_true.shape).astype(float)
+		if no_s_inv:
+			# this is weird, but preserves random seeds, so useful. 
+			# adjust the above draws to have "infinite" noise where 
+			# masked
+			cov_noise = np.zeros((n_spectra, n_bins))
+			for i in range(n_spectra):
+				cov_noise[i, :] = var_noise
+				masked = (mask[i, :] == 0)
+				cov_noise[i, masked] = inf_noise
+				noise[i, masked] *= inf_noise / var_noise[masked]
+		if diagnose:
+			for i in range(n_spectra):
+				mp.plot(noise[i, :], 'k-', alpha=0.3)
+			mp.show()
+
+		# generate data
+		data = mask * (spectra_true + noise)
+		wl = np.arange(n_bins).astype(float)
+		if diagnose:
+			print float(np.sum(mask==1.0)) / float(np.sum(mask==0.0)), 0.86/0.14
+			mp.imshow(data, interpolation='nearest')
+			mp.show()
+
+	# ensure all processes have same data
+	if use_mpi:
+		mpi.COMM_WORLD.Bcast(data, root=0)
+		mpi.COMM_WORLD.Bcast(mask, root=0)
+		if no_s_inv:
+			mpi.COMM_WORLD.Bcast(cov_noise, root=0)
+		else:
+			mpi.COMM_WORLD.Bcast(inv_cov_noise, root=0)
+		mpi.COMM_WORLD.Bcast(wl, root=0)
+		mpi.COMM_WORLD.Bcast(mean, root=0)
+		mpi.COMM_WORLD.Bcast(cov, root=0)
 
 else:
 
@@ -166,6 +235,7 @@ mean_sample = np.mean(data, 0)          # signalpriormean
 cov_sample = np.cov(data, rowvar=False) # SignalCovariance
 mean_samples = np.zeros((n_bins, n_samples))
 cov_samples = np.zeros((n_bins, n_bins, n_samples))
+conds = np.zeros(n_samples)
 
 # Gibbs sample!
 job_list = allocate_jobs(n_spectra, n_procs, rank)
@@ -177,45 +247,78 @@ for i in range(n_samples):
 		print i, '/', n_samples
 
 	# invert current sample covariance
-	inv_cov_sample = npl.inv(cov_sample)
+	if not no_s_inv:
+		inv_cov_sample = npl.inv(cov_sample)
 
 	# calculate WF for each spectrum and use to draw true spectra
 	spectra_samples = np.zeros(data.shape)
 	for j in job_list:
 
-		# check if already have spectrum-dependent noise
-		if len(inv_cov_noise.shape) == 3:
-			inv_cov_noise_j = inv_cov_noise[j, :, :]
+		# avoid S^-1?
+		if no_s_inv:
+
+			# check if have spectrum-dependent noise
+			if len(cov_noise.shape) == 3:
+				cov_noise_j = cov_noise[j, :, :]
+			else:
+				cov_noise_j = np.diag(cov_noise[j, :]) # noisy mask included
+			s_n_inv = npl.inv(cov_sample + cov_noise_j) 
+			mean_wf = np.dot(cov_noise_j, \
+							 np.dot(s_n_inv, mean_sample)) + \
+					  np.dot(cov_sample, \
+							 np.dot(s_n_inv, data[j, :]))
+			cov_wf = np.dot(cov_noise_j, np.dot(s_n_inv, cov_sample))
+			cov_wf = symmetrize(cov_wf)
+
 		else:
-			inv_cov_noise_j = np.outer(mask[j, :], mask[j, :]) * \
-							  inv_cov_noise
-		cov_wf = npl.inv(inv_cov_sample + inv_cov_noise_j)
-		cov_wf = symmetrize(cov_wf)
-		mean_wf = np.dot(cov_wf, \
-						 np.dot(inv_cov_sample, mean_sample) + \
-						 np.dot(inv_cov_noise_j, data[j, :]))
+
+			# check if have spectrum-dependent noise
+			if len(inv_cov_noise.shape) == 3:
+				inv_cov_noise_j = inv_cov_noise[j, :, :]
+			else:
+				inv_cov_noise_j = np.outer(mask[j, :], mask[j, :]) * \
+								  inv_cov_noise
+			cov_wf = npl.inv(inv_cov_sample + inv_cov_noise_j)
+			cov_wf = symmetrize(cov_wf)
+			mean_wf = np.dot(cov_wf, \
+							 np.dot(inv_cov_sample, mean_sample) + \
+							 np.dot(inv_cov_noise_j, data[j, :]))
+			
 		spectra_samples[j, :] = npr.multivariate_normal(mean_wf, \
 														cov_wf, 1)
-	if use_mpi:
-		mpi.COMM_WORLD.barrier()
 	spectra_samples = complete_array(spectra_samples, use_mpi)
 
-	# sample signal mean
-	mean_sample = npr.multivariate_normal(np.mean(spectra_samples, 0), \
-										  cov_sample / n_spectra, 1)[0, :]
+	# sample everything else on master process to avoid race 
+	# conditions
+	if rank == 0:
 
-	# sample signal covariance matrix
-	# NB: scipy.stats uses numpy.random seed, which i've already set
-	n_dof = n_spectra - (1 - jeffreys_prior) * (n_bins + 1)
-	sigma = np.zeros((n_bins, n_bins))
-	for j in range(n_spectra):
-		delta = spectra_samples[j, :] - mean_sample
-		sigma += np.outer(delta, delta)
-	cov_sample = sps.invwishart.rvs(n_dof, sigma, 1)
+		# sample signal mean
+		mean_sample = \
+			npr.multivariate_normal(np.mean(spectra_samples, 0), \
+									cov_sample / n_spectra, 1)[0, :]
 
-	# store samples (marginalize over true spectra)
-	mean_samples[:, i] = mean_sample
-	cov_samples[:, :, i] = cov_sample
+		# sample signal covariance matrix
+		# NB: scipy.stats uses numpy.random seed, which i've already set
+		n_dof = n_spectra - (1 - jeffreys_prior) * (n_bins + 1)
+		sigma = np.zeros((n_bins, n_bins))
+		for j in range(n_spectra):
+			delta = spectra_samples[j, :] - mean_sample
+			sigma += np.outer(delta, delta)
+		cov_sample = sps.invwishart.rvs(n_dof, sigma, 1) + \
+					 np.diag(np.ones(n_bins) * reg_noise)
+
+		# store samples (marginalize over true spectra)
+		mean_samples[:, i] = mean_sample
+		cov_samples[:, :, i] = cov_sample
+        
+	conds[i] = npl.cond(cov_sample)
+	if rank == 0:
+		print conds[i]
+
+	# broadcast required objects to all processes
+	if use_mpi:
+		mpi.COMM_WORLD.Bcast(mean_sample, root=0)
+		mpi.COMM_WORLD.Bcast(cov_sample, root=0)
 
 # summarize results
 mp_mean = np.mean(mean_samples[:, n_warmup:], 1)
@@ -226,7 +329,7 @@ if rank == 0:
 	# selection of trace plots
 	fig, axes = mp.subplots(3, 1, figsize=(8, 5), sharex=True)
 	axes[0].plot(mean_samples[0, :])
-	axes[1].plot(mean_samples[50, :])
+	axes[1].plot(mean_samples[n_bins / 2, :])
 	axes[2].plot(mean_samples[-1, :])
 	axes[2].set_xlabel('sample')
 	axes[1].set_ylabel('mean')
@@ -387,4 +490,13 @@ if rank == 0:
 							top='off', labeltop='off', right='off', \
 							left='off', labelleft='off')
 	mp.savefig('simple_test_low_rank_covariance.pdf', bbox_inches='tight')
+	mp.close()
+
+	# condition numbers of sampled covariance matrices
+	mp.plot(conds)
+        if datafile is None:
+                mp.axhline(npl.cond(cov))
+	mp.xlabel('index')
+	mp.ylabel('condition number')
+	mp.savefig('simple_test_conds.pdf', bbox_inches='tight')
 	mp.close()
